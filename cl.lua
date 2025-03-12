d24c2f8ca897797c0097c313669ae36ff1a83385
kylie201 <normanmacher69@gmail.com>
2025-03-12 18:21:23 +0100

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -1,7 +1,21 @@
 local WhitelistedClientIDs = {
+
+    --brandon
     "CEE26742-23B4-4189-B347-07C7DA28AB31",
+
+    --pase
+    
+
+    --kitkat
     "dea346ad-ec63-49a4-9655-2cef018892c7",
-    "ZZZZZZZZ-ZZZZ-ZZZZ-ZZZZ-ZZZZZZZZZZZZ"
+
+    --emperor11
+    "d3910f6d-4036-46fb-93bd-e2c2632b8bca",
+    "564a4d69-b93b-4661-8906-c8b3997e123e",
+    "41326c23-98ae-4c3e-83fc-d8b344d1ddef"
+
+    
+    
 }
 
 return WhitelistedClientIDs

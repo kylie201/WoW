53eab080a29f8e6696a70662194e6bfaaac4802f
kylie201 <normanmacher69@gmail.com>
2025-03-13 17:29:37 +0100

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -1,38 +1,77 @@
-local WhitelistedClientIDs = {
+local function obfuscate(str)
+    local result = ""
+    for i = #str, 1, -1 do
+        local char = string.sub(str, i, i)
+        local byte = string.byte(char)
+        local shifted = string.char((byte + 7) % 256)
+        result = result .. shifted
+    end
+    return result
+end
+
+local function deobfuscate(str)
+    local result = ""
+    for i = #str, 1, -1 do
+        local char = string.sub(str, i, i)
+        local byte = string.byte(char)
+        local shifted = string.char((byte - 7 + 256) % 256)
+        result = result .. shifted
+    end
+    return result
+end
+
+-- Obfuscated client IDs
+local ObfuscatedIDs = {
     --Brandon
-    "CEE26742-23B4-4189-B347-07C7DA28AB31",
-    "871a6673-5ba4-4113-ad48-cda8891e603d",
+    "26IH(H9IJMK4=UV4=?@P4UVI4YXKJJL(LLJ",
+    "k:7-p,l>hp2RRUX4hko(4(hi\056k9>>h9>>",
     --Pase
     
     --Kitkat
-    "dea346ad-ec63-49a4-9655-2cef018892c7",
-    "988db681-b23b-450f-952d-111ab894345c",
+    "4*j>??7mljw4\\\\]1=UVP4:=-p4kh?7=hlk",
+    "j\\UXVP4m67mW4k\\]\\4q\\]1=s(hi(i>?@",
     --Emperor11
-    "d3910f6d-4036-46fb-93bd-e2c2632b8bca",
-    "564a4d69-b93b-4661-8906-c8b3997e123e",
-    "41326c23-98ae-4c3e-83fc-d8b344d1ddef",
+    "hjio>:oj(jjm4kiu=4imq14:;iw4k=m76>:k",
+    "l:;>2lPjiu=4>==q4=6?@4j6?@+=@@ioUV\\",
+    "mlkk>kU[YPq4l:=U4jmn4k2ot4UVKJJoYXK",
     --Grooda
-    "a3702d8c-1e85-481a-a9aa-1aa1a2065d1d",
-    "9db0b024-0d49-40fa-b4e5-96d03d9c879d",
-    "2128fa89-bab9-4d48-af7a-3e0fc954c5f4",
+    "k>k\\jw(hh>h4hhhn4(>?(4\\?>l4j?k*4;(h",
+    "k=@?j@=UV<K4hmn*4(lui4\\ul(4j@=UV<K=@",
+    "Umj\\j=\\67(4?UVT4h4mh4l6784j\\]\\mo>o>",
     --Raul
-    "309400c6-ec66-4750-ba1c-0ba9866971c6",
-    "c7a7fe2f-790d-4333-8707-11f44ef27cc8",
+    "=j>@===>hio4jh>h4>*4p,4hi>j6?@+==j6",
+    "?jj4*l\\UVS4:;iu4UVVV4>6?>4*lmmUU^PJ",
     --Manny
-    "06e49a0e-0303-45bb-8d6a-345c110cec43",
+    "VUjljw:6:64iipU4hk=?4VUJN>6jljw:6",
     --Demon
-    "dadcb05c-e3cf-4642-ba70-9e0548728260",
+    "6=o?*\\6?@lm\\4YUV=4hi>w4=<hi+w\\jkk",
     --Dino
-    "41a8e49c-6692-4b41-a375-75bd0cbe1ee3",
+    ":ll>liw>pf14>iUV4\\4;(4k2ot4lijwlj>U",
     --Brayan
-    "309400c6-ec66-4750-ba1c-0ba9866971c6",
-    "c1074205-b286-4df8-a987-0d1002c793c5",
-    "02AD0E16-D302-425C-95FA-B834478400B5",
-    "dcc9d1f3-8087-4fde-9520-8fad6eac59f2",
+    "=j>@===>hio4jh>h4>*4p,4hi>j6?@+==j6",
+    "\\j:@AOYXZ=4?mko4@?h4=>6<4o6owj>67j",
+    "\\I67U?*[SXK4jo\\Y4h]\\m4UVV[VYUKJh(6",
+    "om]\\jhlm\\6?4lkm*4=o\\]4?kim4jhlm67j\\",
     --Gato
-    "2d550b47-607e-4308-baf4-8956755b717c",
+    "j4>itw\\lt6=U4?6UV4mhio4\\\\]1=\\\\4*k\\",
     --MCL_Brayan
-    "b404fcd4-5e97-4250-834e-1a892d27a604",
+    "U6=h4o=@hro4w\\]Y4w\\]Y4l:;>4kwr:Umj*"
 }
+
+local function isWhitelisted(clientID)
+    for _, encryptedID in ipairs(ObfuscatedIDs) do
+        if clientID == deobfuscate(encryptedID) then
+            return true
+        end
+    end
+    return false
+end
+
+local WhitelistedClientIDs = {}
+for i, obfuscatedID in ipairs(ObfuscatedIDs) do
+    WhitelistedClientIDs[i] = deobfuscate(obfuscatedID)
+end
+
+WhitelistedClientIDs._verify = isWhitelisted
 
 return WhitelistedClientIDs

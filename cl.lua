62b51e1d9b9ce9c4c184a7dd3aa095d210521649
kylie201 <normanmacher69@gmail.com>
2025-03-13 17:26:15 +0100

    Update cl.lua

--- cl.lua
+++ cl.lua
@@ -1,85 +1,38 @@
-local _a = "CEE267"
-local _b = "42-23B4-4189-B347-07C7DA28AB31"
-local _c = "871a66"
-local _d = "73-5ba4-4113-ad48-cda8891e603d"
-local _e = "dea346"
-local _f = "ad-ec63-49a4-9655-2cef018892c7"
-local _g = "988db6"
-local _h = "81-b23b-450f-952d-111ab894345c"
-local _i = "d3910f"
-local _j = "6d-4036-46fb-93bd-e2c2632b8bca"
-local _k = "564a4d"
-local _l = "69-b93b-4661-8906-c8b3997e123e"
-local _m = "41326c"
-local _n = "23-98ae-4c3e-83fc-d8b344d1ddef"
-local _o = "a3702d"
-local _p = "8c-1e85-481a-a9aa-1aa1a2065d1d"
-local _q = "9db0b0"
-local _r = "24-0d49-40fa-b4e5-96d03d9c879d"
-local _s = "2128fa"
-local _t = "89-bab9-4d48-af7a-3e0fc954c5f4"
-local _u = "309400"
-local _v = "c6-ec66-4750-ba1c-0ba9866971c6"
-local _w = "c7a7fe"
-local _x = "2f-790d-4333-8707-11f44ef27cc8"
-local _y = "06e49a"
-local _z = "0e-0303-45bb-8d6a-345c110cec43"
-local _aa = "dadcb0"
-local _ab = "5c-e3cf-4642-ba70-9e0548728260"
-local _ac = "41a8e4"
-local _ad = "9c-6692-4b41-a375-75bd0cbe1ee3"
-local _ae = "309400"
-local _af = "c6-ec66-4750-ba1c-0ba9866971c6"
-local _ag = "c10742"
-local _ah = "05-b286-4df8-a987-0d1002c793c5"
-local _ai = "02AD0E"
-local _aj = "16-D302-425C-95FA-B834478400B5"
-local _ak = "dcc9d1"
-local _al = "f3-8087-4fde-9520-8fad6eac59f2"
-local _am = "2d550b"
-local _an = "47-607e-4308-baf4-8956755b717c"
-local _ao = "b404fc"
-local _ap = "d4-5e97-4250-834e-1a892d27a604"
-
-local function concat(a, b)
-    return a .. b
-end
-
 local WhitelistedClientIDs = {
     --Brandon
-    concat(_a, _b),
-    concat(_c, _d),
+    "CEE26742-23B4-4189-B347-07C7DA28AB31",
+    "871a6673-5ba4-4113-ad48-cda8891e603d",
     --Pase
     
     --Kitkat
-    concat(_e, _f),
-    concat(_g, _h),
+    "dea346ad-ec63-49a4-9655-2cef018892c7",
+    "988db681-b23b-450f-952d-111ab894345c",
     --Emperor11
-    concat(_i, _j),
-    concat(_k, _l),
-    concat(_m, _n),
+    "d3910f6d-4036-46fb-93bd-e2c2632b8bca",
+    "564a4d69-b93b-4661-8906-c8b3997e123e",
+    "41326c23-98ae-4c3e-83fc-d8b344d1ddef",
     --Grooda
-    concat(_o, _p),
-    concat(_q, _r),
-    concat(_s, _t),
+    "a3702d8c-1e85-481a-a9aa-1aa1a2065d1d",
+    "9db0b024-0d49-40fa-b4e5-96d03d9c879d",
+    "2128fa89-bab9-4d48-af7a-3e0fc954c5f4",
     --Raul
-    concat(_u, _v),
-    concat(_w, _x),
+    "309400c6-ec66-4750-ba1c-0ba9866971c6",
+    "c7a7fe2f-790d-4333-8707-11f44ef27cc8",
     --Manny
-    concat(_y, _z),
+    "06e49a0e-0303-45bb-8d6a-345c110cec43",
     --Demon
-    concat(_aa, _ab),
+    "dadcb05c-e3cf-4642-ba70-9e0548728260",
     --Dino
-    concat(_ac, _ad),
+    "41a8e49c-6692-4b41-a375-75bd0cbe1ee3",
     --Brayan
-    concat(_ae, _af),
-    concat(_ag, _ah),
-    concat(_ai, _aj),
-    concat(_ak, _al),
+    "309400c6-ec66-4750-ba1c-0ba9866971c6",
+    "c1074205-b286-4df8-a987-0d1002c793c5",
+    "02AD0E16-D302-425C-95FA-B834478400B5",
+    "dcc9d1f3-8087-4fde-9520-8fad6eac59f2",
     --Gato
-    concat(_am, _an),
+    "2d550b47-607e-4308-baf4-8956755b717c",
     --MCL_Brayan
-    concat(_ao, _ap),
+    "b404fcd4-5e97-4250-834e-1a892d27a604",
 }
 
 return WhitelistedClientIDs
